2231c29f95b232cb2487c470bd3cbaecb8d4f84e
Matthew King <32280053+crashpro33@users.noreply.github.com>
2019-04-30 18:44:17 -0500

    Update RandomDigits.java

--- RandomDigits.java
+++ RandomDigits.java
@@ -1,12 +1,12 @@
 public static int[] generateRandomDigits(int low, int high, int size) {
-		int[] digits = new int[size];
-		for (int i = 0; i < size; i++) {
-			digits[i] = findRandom(low, high);
-		}
-		return digits;
+	int[] digits = new int[size];
+	for (int i = 0; i < size; i++) {
+		digits[i] = findRandom(low, high);
+	}
+	return digits;
 }
 
 public static int findRandom(int low, int high) {
-    int randomInt = (int)(Math.random() * high) + low;
-    return randomInt;
+	int randomInt = (int)(Math.random() * high) + low;
+	return randomInt;
 }
